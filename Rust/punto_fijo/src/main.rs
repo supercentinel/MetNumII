19300b439a83dc8fd4cdd2bc7a70083b412b73f1
ALaN <digitalforce.ac@gmail.com>
2023-02-10 22:59:12 -0600

    refactored struct Punto & fn punto_fijo

--- Rust/punto_fijo/src/main.rs
+++ Rust/punto_fijo/src/main.rs
@@ -1,73 +1,57 @@
-use std::num;
-
 struct Punto {
     x: f64,
     y: f64,
 }
 
+impl Punto
+{
+    fn new(_x:f64, _y:f64) -> Self
+    {
+        Self {
+            x: _x,
+            y: _y,
+        }
+    }
+
+    fn distancia(&self, otro: &Punto) -> f64
+    {
+        return ((self.x - otro.x).powi(2) + (self.y - otro.y).powi(2)).sqrt(); 
+    }
+}
+
 fn g_1(x: f64, y: f64) -> f64
 {
-    ((x * x) - y + 0.5)/2.0
+    return -(((x * x) - y - 2.0 - (4.0 * x))/(4.0));
 }
 
 fn g_2(x: f64, y: f64) -> f64
 {
-    (-(x * x) - (4.0 * (y * y)) + (8.0 * y) + 4.0)/(8.0)
+    return -(((2.0 * x * y) - 3.0 - (5.0 * y))/(5.0));
 }
 
 fn punto_fijo(x_0: f64, y_0: f64, tolerancia: f64, iteraciones: u32) -> Punto
-{
-    let mut p = Punto
-    {
-        x: 0.0,
-        y: 0.0,
-    };
-    let mut pe = Punto
-    {
-        x: 0.0,
-        y: 0.0,
-    };
-
+{     
     let mut k: u32 = 0;
-    let mut x_k: f64 = 0.0;
-    let mut y_k: f64 = 0.0;
-    let mut e_a: f64;
-
-    p.x = x_0;
-    p.y = y_0;
+    let mut e_a: f64 = 1.0;
+    let mut p: Punto = Punto::new(x_0, y_0);
+    let mut p_km1: Punto = Punto::new(0.0, 0.0);
 
     println!("|{:<4}|{:<25}|{:<25}|{:<25}|", "k", "x_k", "y_k", "E_a");
 
     loop
     {
-        pe.x = p.x - x_k;
-        pe.y = p.y - y_k;
+        p_km1.x = p.x;
+        p_km1.y = p.y;
 
-        x_k = p.x;
-        y_k = p.y;
-
-        if pe.x == 0.0
-        {
-            pe.y = pe.y.powi(2);
-            e_a = pe.y.sqrt();
-        }else if pe.y == 0.0
-        {
-            pe.x = pe.x.powi(2);
-            e_a = pe.x.sqrt();
-        }else
-        {
-            pe.y = pe.y.powi(2);
-            pe.x = pe.x.powi(2);
-            e_a = (pe.x + pe.y).sqrt();
-        }
-
-        println!("|{:<4}|{:<25}|{:<25}|{:<25}|", k, x_k, y_k, e_a);
+        println!("|{:<4}|{:<25}|{:<25}|{:<25}|", k, p.x, p.y, e_a);
 
         if k >= iteraciones {break};
         if e_a < tolerancia {break};
 
-        p.x = g_1(x_k, y_k);
-        p.y = g_2(x_k, y_k);
+        p.x = g_1(p_km1.x, p_km1.y);
+        p.y = g_2(p_km1.x, p_km1.y);
+
+        e_a = p.distancia(&p_km1);
 
         k += 1;
     }
@@ -75,5 +59,5 @@ fn punto_fijo(x_0: f64, y_0: f64, tolerancia: f64, iteraciones: u32) -> Punto
     return p;
 }
 fn main() {
-    let p = punto_fijo(0.0, 1.0, 0.000005, 10);
+    punto_fijo(1.75, 1.0, 0.00008, 100);
 }
