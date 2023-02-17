d357651650fbd9c2f985ec5a3403e9dfc82a1bfd
AlaN <digitalforce.ac@gmail.com>
2023-02-16 20:00:40 -0600

    formatted print added

--- Julia/punto_fijo_sucesivo.jl
+++ Julia/punto_fijo_sucesivo.jl
@@ -1,3 +1,5 @@
+using Printf
+
 mutable struct Punto
     x::Float64
     y::Float64
@@ -21,11 +23,13 @@ function punto_fijo_sucesivo(x::Float64, y::Float64, tolerancia::Float64, iterac
     k::UInt32 = 0.0
     e_a::Float64 = 1.0
 
+    @printf("|%-4s|%-24s|%-24s|%-24s|\n", "k", "x^k", "y^k", "E_a")
+
     while true
         pkm1.x = p.x
         pkm1.y = p.y
 
-        println(k, "|", p.x, "|", p.y, "|", e_a)
+        @printf("|%-4i|%-24.20LF|%-24.20LF|%-24.20LF|\n", k, p.x, p.y, e_a)
 
         if k >= iteraciones 
             break
