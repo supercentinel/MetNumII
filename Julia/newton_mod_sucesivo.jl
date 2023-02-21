482f053f3b9ce94e4fe4431c1095030dc677ffb1
AlaN <digitalforce.ac@gmail.com>
2023-02-21 11:51:10 -0600

    moddified for portafolio#3. Does not converge

--- Julia/newton_mod_sucesivo.jl
+++ Julia/newton_mod_sucesivo.jl
@@ -1,21 +1,28 @@
 #using LinearAlgebra
 using Printf
 
-function f1(x::Float64, y::Float64)
-    return (x ^ 2.0) - (10.0 * x) + (y ^ 2.0) + 6.0
+function f1(x::Float64, y::Float64, z::Float64)
+    return (x ^ 2) + y + (2 * (z ^ 2)) - 10.0
 end
 
-function f2(x::Float64, y::Float64)
-    return (x * (y ^ 2)) + x - (10.0 * y) + 4.0
+function f2(x::Float64, y::Float64, z::Float64)
+    return (5 * x) - (6 * y) + z
 end
 
-function f1y(x::Float64, y::Float64)
-    return (2.0 * y)
+function f3(x::Float64, y::Float64, z::Float64)
+    return z - (x ^ 2) - (y ^ 2)
 end
-
-
-function f2x(x::Float64, y::Float64)
-    return (y * y) + 1.0
+#con respecto a z
+function f1d(x::Float64, y::Float64, z::Float64)
+    return 4.0 * z
+end
+#con respecto a x
+function f2d(x::Float64, y::Float64, z::Float64)
+    return 5.0
+end
+#con respecto a z
+function f3d(x::Float64, y::Float64, z::Float64)
+    return 1.0
 end
 
 #En la libería estándar existe en LinearAlgebra la función norma que hace lo mismo. Meh.
@@ -34,8 +41,8 @@ end
 function newton_mod_suc(X::Array{Float64}, tolerancia::Float64, iteraciones::Int64)
 
     X_k = X
-    F_X_k_ = [0.0, 0.0]
-    δF_X_k_ = [0.0, 0.0]
+    F_X_k_ = [0.0, 0.0, 0.0]
+    δF_X_k_ = [0.0, 0.0, 0.0]
     e_a = 0.0
     k = 0
 
@@ -43,23 +50,33 @@ function newton_mod_suc(X::Array{Float64}, tolerancia::Float64, iteraciones::Int
 
     while true
 
-        F_X_k_[1] = f2(X_k[1], X_k[2])
-        δF_X_k_[1] = f2x(X_k[1], X_k[2])
+        F_X_k_[1] = f1(X_k[1], X_k[2], X_k[3])
+        δF_X_k_[1] = f1d(X_k[1], X_k[2], X_k[3])
 
         X_k[1] = X_k[1] - ((F_X_k_[1])/(δF_X_k_[1]))
 
 
-        F_X_k_[2] = f1(X_k[1], X_k[2])
-        δF_X_k_[2] = f1y(X_k[1], X_k[2])
+        F_X_k_[2] = f2(X_k[1], X_k[2], X_k[3])
+        δF_X_k_[2] = f2d(X_k[1], X_k[2], X_k[3])
 
         X_k[2] = X_k[2] - ((F_X_k_[2])/(δF_X_k_[2]))
 
+        F_X_k_[3] = f3(X_k[1], X_k[2], X_k[3])
+        δF_X_k_[3] = f3d(X_k[1], X_k[2], X_k[3])
+
+        X_k[3] = X_k[3] - ((F_X_k_[3])/(δF_X_k_[3]))
+
         e_a = norma(F_X_k_)
 
+        print("F(X^k) = ")
         println(F_X_k_)
+        print("δF(X^k) = ")
         println(δF_X_k_)
+        print("Error = ")
         println(e_a)
+        print("k = ")
         println(k)
+        print("X^k = ")
         println(X_k)
 
 
