0cd19029ad7ba766426a9b859f0bfe6abd73744b
ALaN <digitalforce.ac@gmail.com>
2023-02-20 18:11:24 -0600

    tried with all partial derivatives. does not converge

--- Julia/newton_mod.jl
+++ Julia/newton_mod.jl
@@ -2,7 +2,7 @@
 using Printf
 
 function f1(x::Float64, y::Float64, z::Float64)
-    return (x ^ 2) + (2 * (z ^ 2)) + y - 10.0
+    return (x ^ 2) + y + (2 * (z ^ 2)) - 10.0
 end
 
 function f2(x::Float64, y::Float64, z::Float64)
@@ -14,15 +14,15 @@ function f3(x::Float64, y::Float64, z::Float64)
 end
 #con respecto a z
 function f1d(x::Float64, y::Float64, z::Float64)
-    return (4 * z)
-end
-#con respecto a x
-function f2d(x::Float64, y::Float64, z::Float64)
-    return 5
+    return 4 * (z ^ 2)
 end
 #con respecto a y
+function f2d(x::Float64, y::Float64, z::Float64)
+    return -6.0
+end
+#con respecto a x
 function f3d(x::Float64, y::Float64, z::Float64)
-    return -(2 * y)
+    return -(2 * x)
 end
 
 #En la libería estándar existe en LinearAlgebra la función norma que hace lo mismo. Meh.
@@ -47,6 +47,7 @@ function newton_mod(X::Array{Float64}, tolerancia::Float64, iteraciones::Int64)
     e_a = 1.0
     k = 0
 
+    print("X = ")
     println(X_km1)
 
     while true
@@ -57,12 +58,14 @@ function newton_mod(X::Array{Float64}, tolerancia::Float64, iteraciones::Int64)
         F_X_k_[2] = f2(X_km1[1], X_km1[2], X_km1[3])
         F_X_k_[3] = f3(X_km1[1], X_km1[2], X_km1[3])
 
+        print("F(X) = ")
         println(F_X_k_)
 
         δF_X_k_[1] = f1d(X_km1[1], X_km1[2], X_km1[3])
         δF_X_k_[2] = f2d(X_km1[1], X_km1[2], X_km1[3])
         δF_X_k_[3] = f3d(X_km1[1], X_km1[2], X_km1[3])
 
+        print("δF(X) = ")
         println(δF_X_k_)
 
         e_a = norma(F_X_k_)
@@ -80,7 +83,9 @@ function newton_mod(X::Array{Float64}, tolerancia::Float64, iteraciones::Int64)
         X_k[2] = X_km1[2] - ((F_X_k_[2])/(δF_X_k_[2]))
         X_k[3] = X_km1[3] - ((F_X_k_[3])/(δF_X_k_[3]))
 
+        print("K = ")
         println(k)
+        print("X^k = ")
         println(X_k)
 
         k += 1
