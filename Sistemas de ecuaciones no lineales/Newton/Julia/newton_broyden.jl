0e7703bb3e7836bc1712c276d232d66e935bfbdc
4L4N <digitalforce.ac@gmail.com>
2023-02-27 10:57:06 -0600

    added

---
+++ Sistemas de ecuaciones no lineales/Newton/Julia/newton_broyden.jl
@@ -0,0 +1,112 @@
+using Printf
+
+function f1(x::Float64, y::Float64)
+    return (x ^ 2) - (10.0 * x) + (y ^ 2) + 8.0
+end
+
+function f2(x::Float64, y::Float64)
+    return (x * (y ^ 2)) + x - (10.0 * y) + 8.0
+end
+
+function f1x(x::Float64, y::Float64)
+    return (2 * x) - 10.0
+end
+
+function f1y(x::Float64, y::Float64)
+    return 2.0 * y
+end
+
+function f2x(x::Float64, y::Float64)
+    return (y ^ 2) + 1
+end
+
+function f2y(x::Float64, y::Float64)
+    return (2 * x * y) - 10.0
+end
+
+function Error_a(A_k::Array{Float64}, A_km1::Array{Float64})
+    return √(((A_k[1] - A_km1[1]) ^ 2)+((A_k[2] - A_km1[2]) ^ 2))
+end
+
+function Norma(A::Array{Float64})
+    return √(((A[1]) ^ 2)+((A[2]) ^ 2))
+end
+
+function newton_broyden(A::Array{Float64}, tolerancia::Float64, iteraciones::Int64)
+    A_k = A
+    A_km1 = A
+    J = [f1x f1y; f2x f2y]
+    F = [f1 f2]
+    J_xk_ = zeros(Float64, 2 , 2)
+    F_xkm1_ = zeros(Float64, 2, 1)
+    F_xk_ = zeros(Float64, 2, 1)
+    J_inv = zeros(Float64, 2, 2)
+    ΔX = zeros(Float64, 2, 1)
+    ΔF_X_ = zeros(Float64, 2, 1);
+    She_Morr = zeros(Float64, 2, 2)
+    e_r = 1.0
+    k = 0
+
+    for row in 1:2
+        F_xkm1_[row] = F[row](A_k[1], A_k[2])
+    end
+
+    for row in 1:2
+        for column in 1:2
+            J_xk_[row,column] = J[row,column](A_k[1], A_k[2])
+         end
+    end
+
+    J_inv = inv(J_xk_)
+
+    A_k = A_k - (J_inv * F_xkm1_)
+
+    for row in 1:2
+        F_xk_[row] = F[row](A_k[1], A_k[2])
+    end
+
+    ΔX= A_k - A_km1
+    ΔF_X_ = F_xk_ - F_xkm1_
+
+    denom = transpose(ΔX) * J_inv * ΔF_X_
+    tru_den = denom[1]
+
+    She_Morr = (ΔX - (J_inv * ΔF_X_)) * (transpose(ΔX) * J_inv)
+#=
+    for i in 1:2
+        for j in 1:2
+            She_Morr[i][j] = She_Morr[i][j]/2
+        end
+    end
+=#
+
+    She_Morr = She_Morr * 1/tru_den
+
+    She_Morr = J_inv - She_Morr
+
+    print("X^k-1 = ")
+    println(A_km1)
+    print("X^k = ")
+    println(A_k)
+    print("F(X^k-1) = ")
+    println(F_xkm1_)
+    print("F(X^k) = ")
+    println(F_xk_)
+    print("ΔX = ")
+    println(ΔX)
+    print("ΔF(X) = ")
+    println(ΔF_X_)
+    print("Sherman-Morrison = ")
+    println(She_Morr)
+    print("Denom = ")
+    println(denom)
+
+    return A_k
+end
+
+function main()
+    A = [2.0, 3.0]
+    R = newton_broyden(A, 0.005, 10)
+end
+
+main()
