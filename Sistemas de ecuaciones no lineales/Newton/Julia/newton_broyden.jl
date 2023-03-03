32efb704474039bf0177d9d87c74cb1606044fe4
ALaN <digitalforce.ac@gmail.com>
2023-03-02 22:58:20 -0600

    now method & eq.system are separated

--- Sistemas de ecuaciones no lineales/Newton/Julia/newton_broyden.jl
+++ Sistemas de ecuaciones no lineales/Newton/Julia/newton_broyden.jl
@@ -1,60 +1,37 @@
 using Printf
 
-function f1(x::Float64, y::Float64)
-    return (x ^ 2) - (10.0 * x) + (y ^ 2) + 8.0
-end
-
-function f2(x::Float64, y::Float64)
-    return (x * (y ^ 2)) + x - (10.0 * y) + 8.0
-end
-
-function f1x(x::Float64, y::Float64)
-    return (2 * x) - 10.0
-end
-
-function f1y(x::Float64, y::Float64)
-    return 2.0 * y
-end
-
-function f2x(x::Float64, y::Float64)
-    return (y ^ 2) + 1
-end
-
-function f2y(x::Float64, y::Float64)
-    return (2 * x * y) - 10.0
-end
-
-function Error_a(A_k::Array{Float64}, A_km1::Array{Float64})
-    return √(((A_k[1] - A_km1[1]) ^ 2)+((A_k[2] - A_km1[2]) ^ 2))
-end
-
 function Norma(A::Array{Float64})
-    return √(((A[1]) ^ 2)+((A[2]) ^ 2))
-end
+    ∑= 0.0
 
-function newton_broyden(A::Array{Float64}, tolerancia::Float64, iteraciones::Int64)
-    A_k = copy(A)
-    A_km1 = copy(A)
-    J = [f1x f1y; f2x f2y]
-    F = [f1 f2]
-    J_xk_ = zeros(Float64, 2 , 2)
-    F_xkm1_ = zeros(Float64, 2, 1)
-    F_xk_ = zeros(Float64, 2, 1)
-    J_inv = zeros(Float64, 2, 2)
-    ΔX = zeros(Float64, 2, 1)
-    ΔF_X_ = zeros(Float64, 2, 1);
-    She_Morr = zeros(Float64, 2, 2)
-    She_Morr_km1 = zeros(Float64, 2, 2)
-    e_r = 1.0
-    k = 0
-    #inicio de la primra iteración usando la matriz Jacobiana inversa
-    for row in 1:2
-        F_xkm1_[row] = F[row](A_k[1], A_k[2])
+    for i in 1:length(A)
+        ∑ += (A[i] ^ 2)
     end
 
-    for row in 1:2
-        for column in 1:2
-            J_xk_[row,column] = J[row,column](A_k[1], A_k[2])
+    return √∑
+end
+
+function newton_broyden(A::Array{Float64}, F::Array{Function},J::Array{Function}, tolerancia::Float64, iteraciones::Int64)
+    A_k = copy(A)
+    A_km1 = copy(A)
+    J_xk_ = zeros(Float64, length(F) , length(F))
+    F_xkm1_ = zeros(Float64, length(F), 1)
+    F_xk_ = copy(F_xkm1_)
+    J_inv = copy(J_xk_)
+    ΔX = copy(A)
+    ΔF_X_ = copy(F_xk_)
+    She_Morr = copy(J_xk_)
+    She_Morr_km1 = copy(She_Morr)
+    e_r = 1.0
+    k = 1
+
+    #inicio de la primra iteración usando la matriz Jacobiana inversa
+    for row in 1:length(F)
+        F_xkm1_[row] = F[row](A_k)
+    end
+
+    for row in 1:size(J, 1)
+        for column in 1:size(J, 1)
+            J_xk_[row,column] = J[row,column](A_k)
          end
     end
 
@@ -62,8 +39,8 @@ function newton_broyden(A::Array{Float64}, tolerancia::Float64, iteraciones::Int
 
     A_k = A_k - (J_inv * F_xkm1_)
 
-    for row in 1:2
-        F_xk_[row] = F[row](A_k[1], A_k[2])
+    for row in 1:length(F)
+        F_xk_[row] = F[row](A_k)
     end
 
     ΔX= A_k - A_km1
@@ -73,18 +50,19 @@ function newton_broyden(A::Array{Float64}, tolerancia::Float64, iteraciones::Int
 
     She_Morr = (ΔX - (J_inv * ΔF_X_)) * (transpose(ΔX) * J_inv)
     She_Morr = She_Morr * 1/denom[1]
-    She_Morr = J_inv - She_Morr
+    She_Morr = J_inv + She_Morr
+
+    e_r = Norma(F_xk_)
 
     #fin de la primera iteración. Ahora podemos continuar sin la matriz Jacobiana
-
     print("k = ")
     println(k)
     print("X^k-1 = ")
     println(A_km1)
-    print("X^k = ")
-    println(A_k)
     print("F(X^k-1) = ")
     println(F_xkm1_)
+    print("X^k = ")
+    println(A_k)
     print("F(X^k) = ")
     println(F_xk_)
     print("ΔX = ")
@@ -95,7 +73,8 @@ function newton_broyden(A::Array{Float64}, tolerancia::Float64, iteraciones::Int
     println(She_Morr)
     print("Denom = ")
     println(denom)
-
+    print("Error = ")
+    println(e_r)
     k += 1
 
     while true
@@ -105,8 +84,8 @@ function newton_broyden(A::Array{Float64}, tolerancia::Float64, iteraciones::Int
 
         A_k = A_km1 - (She_Morr_km1 * F_xkm1_)
 
-        for row in 1:2
-            F_xk_[row] = F[row](A_k[1], A_k[2])
+        for row in 1:length(F)
+            F_xk_[row] = F[row](A_k)
         end
 
         ΔX = A_k - A_km1
@@ -118,7 +97,7 @@ function newton_broyden(A::Array{Float64}, tolerancia::Float64, iteraciones::Int
 
         She_Morr = She_Morr * 1/denom[1]
 
-        She_Morr = She_Morr_km1 - She_Morr
+        She_Morr = She_Morr_km1 + She_Morr
 
         e_r = Norma(F_xk_)
 
@@ -157,12 +136,3 @@ function newton_broyden(A::Array{Float64}, tolerancia::Float64, iteraciones::Int
 
     return A_k
 end
-
-function main()
-    A = [2.0, 3.0]
-    R = newton_broyden(A, 0.005, 10)
-    print("Resultado = ")
-    println(R)
-end
-
-main()
---
+++ Sistemas de ecuaciones no lineales/Newton/Julia/portafolio_4.jl
@@ -0,0 +1,60 @@
+include("newton_broyden.jl")
+
+function f1(X::Array{Float64})
+    return (X[1] ^ 2) - X[1] + (2 * (X[2] ^ 2)) + (X[2] * X[3]) - 10
+end
+
+function f2(X::Array{Float64})
+    return (5 * X[1]) - (6 * X[2]) + X[3]
+end
+
+function f3(X::Array{Float64})
+    return X[3] - (X[1] ^ 2) - (X[2] ^ 2)
+end
+
+function f1x(X::Array{Float64})
+    return (2.0 * X[1]) -1
+end
+
+function f1y(X::Array{Float64})
+    return (4.0 * X[2]) + X[3]
+end
+
+function f1z(X::Array{Float64})
+    return X[2]
+end
+
+function f2x(X::Array{Float64})
+    return 5.0
+end
+
+function f2y(X::Array{Float64})
+    return -6.0
+end
+
+function f2z(X::Array{Float64})
+    return 1.0
+end
+
+function f3x(X::Array{Float64})
+    return -2.0 * X[1]
+end
+
+function f3y(X::Array{Float64})
+    return -2.0 * X[2]
+end
+
+function f3z(X::Array{Float64})
+    return 1.0
+end
+
+function main()
+    #A = [1.15, 1.5, 3.5]
+    A = [1.13, 1.23, 3.0]
+    J = [f1x f1y f1z; f2x f2y f2z; f3x f3y f3z]
+    F = [f1 f2 f3]
+    R = newton_broyden(A, F, J, 0.00005, 10)
+    println(R)
+end
+
+main()
