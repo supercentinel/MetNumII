d352e40dda38adefd201408e3fabd8ea5f067499
ALaN <digitalforce.ac@gmail.com>
2023-03-05 14:17:11 -0600

    generalized for any eq system

--- Sistemas de ecuaciones no lineales/Punto Fijo/Julia/punto_fijo.jl
+++ Sistemas de ecuaciones no lineales/Punto Fijo/Julia/punto_fijo.jl
@@ -1,58 +1,54 @@
-using Printf
+function distancia(X::Array{Float64}, X_2::Array{Float64})
+    ∑ = 0.0
 
-mutable struct Punto
-    x::Float64
-    y::Float64
+    if length(X) != length(X_2)
+        return 0.0
+    end
+
+    for i ∈ 1:length(X)
+        ∑ += (X[i] - X_2[i]) ^ 2
+    end
+
+    return √∑
 end
 
-function g_1(x::Float64, y::Float64)
-    return ((x ^ 2)- (y ^ 2) - x - 3 + (3 * x))/(3)
-end
-
-function g_2(x::Float64, y::Float64)
-    return -(x + (2 * y) + 1 - (3 * y))/(3)
-end
-
-function distancia(p_1::Punto, p_2::Punto)
-    return √(((p_1.x - p_2.x) ^ 2) + ((p_1.y - p_2.y) ^ 2))
-end
-
-function punto_fijo(x::Float64, y::Float64, tolerancia::Float64, iteraciones::Int64)
-    p = Punto(x, y)
-    pkm1 = Punto(0.0, 0.0)
-    k::UInt32 = 0.0
+function punto_fijo(X::Array{Float64}, G::Array{Function}, tolerancia::Float64, iteraciones::Int64)
+    X_k = copy(X)
+    X_km1 = copy(X)
+    k::Int64 = 0.0
     e_a::Float64 = 1.0
 
-    @printf("|%-4s|%-24s|%-24s|%-24s|\n", "k", "x^k", "y^k", "E_a")
+    print("k = ")
+    println(k)
+    print("X = ")
+    println(X_k)
     #println("|-|-|-|-|")
-    
+
     while true
-        pkm1.x = p.x
-        pkm1.y = p.y
+        X_km1 = copy(X_k)
 
-        @printf("|%-4i|%-24.20LF|%-24.20LF|%-24.20LF|\n", k, p.x, p.y, e_a)
-
-        if k >= iteraciones
-            break
+        for i ∈ 1:length(G)
+            X_k[i] = G[i](X_km1)
         end
+
+        e_a = distancia(X_k, X_km1)
+
+        print("k = ")
+        println(k)
+        print("X = ")
+        println(X_k)
+        print("Error  = ")
+        println(e_a)
+
         if e_a <= tolerancia
             break
         end
-
-        p.x = g_1(pkm1.x, pkm1.y)
-        p.y = g_2(pkm1.x, pkm1.y)
-
-        e_a = distancia(p, pkm1)
+        if k >= iteraciones
+            break
+        end
 
         k += 1
     end
 
-    return p
+    return X_k
 end
-
-
-function main()
-    p = punto_fijo(0.0, 0.0,0.0005, 100)
-end
-
-main()
