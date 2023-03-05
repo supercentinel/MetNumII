2761cde619968672925e0f5322d81968290becc8
ALaN <digitalforce.ac@gmail.com>
2023-03-05 15:22:48 -0600

    refactored for any eq system

--- Sistemas de ecuaciones no lineales/Newton/Newton_Mod/Julia/newton_mod_sucesivo.jl
+++ Sistemas de ecuaciones no lineales/Newton/Newton_Mod/Julia/newton_mod_sucesivo.jl
@@ -1,30 +1,4 @@
 #using LinearAlgebra
-using Printf
-
-function f1(x::Float64, y::Float64, z::Float64)
-    return (x ^ 2) + y + (2 * (z ^ 2)) - 10.0
-end
-
-function f2(x::Float64, y::Float64, z::Float64)
-    return (5 * x) - (6 * y) + z
-end
-
-function f3(x::Float64, y::Float64, z::Float64)
-    return z - (x ^ 2) - (y ^ 2)
-end
-#con respecto a z
-function f1d(x::Float64, y::Float64, z::Float64)
-    return 4.0 * z
-end
-#con respecto a x
-function f2d(x::Float64, y::Float64, z::Float64)
-    return 5.0
-end
-#con respecto a z
-function f3d(x::Float64, y::Float64, z::Float64)
-    return 1.0
-end
-
 #En la libería estándar existe en LinearAlgebra la función norma que hace lo mismo. Meh.
 function norma(A::Array{Float64})
     ∑ = 0.0
@@ -34,15 +8,13 @@ function norma(A::Array{Float64})
     end
 
     return √∑
-
 end
-
 #Usando despazamientos sucesivos
-function newton_mod_suc(X::Array{Float64}, tolerancia::Float64, iteraciones::Int64)
+function newton_mod_suc(X::Array{Float64}, F::Array{Function}, δF::Array{Function}, tolerancia::Float64, iteraciones::Int64)
 
-    X_k = X
-    F_X_k_ = [0.0, 0.0, 0.0]
-    δF_X_k_ = [0.0, 0.0, 0.0]
+    X_k = copy(X)
+    F_X_k_ = zeros(Float64, length(F), 1)
+    δF_X_k_ = zeros(Float64, length(F), 1)
     e_a = 0.0
     k = 0
 
@@ -50,6 +22,13 @@ function newton_mod_suc(X::Array{Float64}, tolerancia::Float64, iteraciones::Int
 
     while true
 
+        for i ∈ 1:length(F)
+            F_X_k_[i] = F[i](X_k)
+            δF_X_k_[i] = δF[i](X_k)
+
+            X_k[i] = X_k[i] - ((F_X_k_[i])/(δF_X_k_[i]))
+        end
+        #=
         F_X_k_[1] = f1(X_k[1], X_k[2], X_k[3])
         δF_X_k_[1] = f1d(X_k[1], X_k[2], X_k[3])
 
@@ -65,6 +44,7 @@ function newton_mod_suc(X::Array{Float64}, tolerancia::Float64, iteraciones::Int
         δF_X_k_[3] = f3d(X_k[1], X_k[2], X_k[3])
 
         X_k[3] = X_k[3] - ((F_X_k_[3])/(δF_X_k_[3]))
+        =#
 
         e_a = norma(F_X_k_)
 
@@ -79,7 +59,6 @@ function newton_mod_suc(X::Array{Float64}, tolerancia::Float64, iteraciones::Int
         print("X^k = ")
         println(X_k)
 
-
         if  e_a <= tolerancia
             break
         end
@@ -91,15 +70,4 @@ function newton_mod_suc(X::Array{Float64}, tolerancia::Float64, iteraciones::Int
     end
 
     return X_k
-
 end
-#=
-function main()
-    A = [0.5, 0.5]
-    println(A)
-    R = newton_mod(A, 0.00005, 10)
-    println(R)
-end
-
-main()
-=#
