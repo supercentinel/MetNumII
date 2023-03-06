a8ef6203088c9d516ec1cae2b698b44e810805af
4L4N <digitalforce.ac@gmail.com>
2023-03-06 11:24:16 -0600

    prototype added

--- Interpolación polinomial/Fórmula de Lagrange/Julia/lagrange.jl
+++ Interpolación polinomial/Fórmula de Lagrange/Julia/lagrange.jl
@@ -1,6 +1,24 @@
-struct punto
+struct Punto
     x::Float64
     y::Float64
 end
 
-function
+function lagrange(puntos::Array{Punto}, estimar::Punto, grado::Int64)
+    estimacion::Float64 = 0.0
+
+    for i ∈ 1:grado
+        estimacion += 1.0
+    end
+
+    return estimacion
+end
+
+pt_1 = Punto(1.0, 1.0)
+
+pt_2 = Punto(1.0, 2.0)
+
+pts = [pt_1, pt_2]
+
+r = lagrange(pts, pt_1, 2)
+println(r)
+display(pts)
