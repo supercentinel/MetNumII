09ee28a26c26676b41462eddccb294dbc3b7fd29
AlaN <digitalforce.ac@gmail.com>
2023-03-11 23:29:49 -0600

    It works! Testing for grades > 3 required

--- Interpolación polinomial/Fórmula de Lagrange/Julia/lagrange.jl
+++ Interpolación polinomial/Fórmula de Lagrange/Julia/lagrange.jl
@@ -3,7 +3,7 @@ struct Punto
     y::Float64
 end
 
-function lagrange_1(puntos::Array{Punto}, p_0::Punto, p::Punto, p_1::Punto, grado::Int64)
+function lagrange_1(p_0::Punto, p_1::Punto, p::Punto)
     estimacion::Float64 = 0.0
 
     estimacion = (((p.x-p_1.x)/(p_0.x-p_1.x))*p_0.y)+(((p.x-p_0.x)/(p_1.x-p_0.x))*p_1.y)
@@ -20,23 +20,54 @@ function lagrange_2(p_0::Punto, p_1::Punto, p_2::Punto, est::Float64)
 end
 function lagrange_n(puntos::Array{Punto}, est::Float64, grado::Int64)
     estimacion::Float64 = 0.0
-    pos::Int64
-    L = zeros(Float64 , grado+1, 1)
-
-    #suponiendo que el arreglo de puntos está ordenado
+    L::Float64 = 1.0
+    ubik = 0
+    
+    if grado > length(puntos)
+        println("El grado maximo del polinomio es n siendo el tamaño del arreglo de puntos n+1")
+        return 0.0
+    end
+    #=
     for i ∈ 1:length(puntos)
-        if p.x > puntos[i].x
-            pos = i
+        if est < puntos[i].x
+            ubik = i-1
+            println(puntos[i-1].x, " > ", est, " > ",puntos[i].x)
             break
         end
     end
+    =#
 
-    for i ∈ 1:grado
-        L *= (est - puntos[i].x)/()
-    end
+    for i ∈ 1:grado+1
+        #println("i = ", i)
+        L = 1.0
+        
+        for j ∈ 1:grado
+            #println("j = ",j)
+            if i == j
+                #=
+                if i == grado+1
+                    L *= (est - puntos[1].x)/(puntos[i].x - puntos[1].x)
+                else
+                end
+                    =#
+                    L *= (est - puntos[grado+1].x)/(puntos[i].x - puntos[grado+1].x)
+                    println(est, " - ", puntos[grado+1].x)
+                    println("-----------")
+                    println(puntos[i].x, " - ", puntos[grado+1].x)
+                    
+                continue
+            end
 
-    for i ∈ 1:grado
-        estimacion += L[i] * puntos[i-1].y
+            #println(est, " - ", puntos[j].x, "/", puntos[i].x, " - ", puntos[j].x)
+            println(est, " - ", puntos[j].x)
+            println("-----------")
+            println(puntos[i].x, " - ", puntos[j].x)
+            L *= (est - puntos[j].x)/(puntos[i].x - puntos[j].x)
+            #println(L)
+        end
+        
+        println(" * ", puntos[i].y)
+        estimacion += L * puntos[i].y
     end
 
     return estimacion
@@ -48,11 +79,21 @@ function main()
     p_2 = Punto(2.61, 7.8)
     p_3 = Punto(5.22, 2.4)
     p_4 = Punto(8.26, -1.7)
+
+    pasr = Punto(2.5, 0)
+
+    puntos = [p_1, p_2]
+
     est = 2.5
 
+    r = lagrange_n(puntos, est, 1)
+    println("Usando la función para n ", est, " ⭇ ", r)
 
-    r = lagrange_2(p_1, p_2, p_3, est)
-    println(est, " ≈ ", r)
+    r_1 = lagrange_1(p_1, p_2, pasr)
+    println(r_1)
+
+    r_3 = lagrange_2(p_1, p_2, p_3, est)
+    println(est, " ⭇ ", r_3)
 end
 
 main()
