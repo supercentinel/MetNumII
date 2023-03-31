f0c1eff331f797d6c88b73f567f7baea7fce0c72
AlaN <digitalforce.ac@gmail.com>
2023-03-30 19:20:46 -0600

    modified for portafolio #7

--- Interpolación polinomial/Diferencias Newton/Julia/dp_ejemplo.jl
+++ Interpolación polinomial/Diferencias Newton/Julia/dp_ejemplo.jl
@@ -2,16 +2,17 @@ include("diferencias_prog.jl")
 
 function main()
 
-    p_0 = Punto(1.0, 0.765198)
-    p_1 = Punto(1.3, 0.620086)
-    p_2 = Punto(1.6, 0.455402)
-    p_3 = Punto(1.9, 0.281819)
-    p_4 = Punto(2.2, 0.110362)
+    p_0 = Punto(50, 24.94)
+    p_1 = Punto(60, 20.11)
+    p_2 = Punto(70, 36.05)
+    p_3 = Punto(80, 23.84)
+    p_4 = Punto(90, 50.57)
+    p_5 = Punto(100, 59.30)
 
-    puntos = [p_0, p_1, p_2, p_3, p_4]
-    est = 1.1
+    puntos = [p_0, p_1, p_2, p_3, p_4, p_5]
+    est = 57.0
 
-    r = diferencias_newton(puntos, est, 4)
+    r = diferencias_newton(puntos, est, 2)
     println()
     println(r)
 end
