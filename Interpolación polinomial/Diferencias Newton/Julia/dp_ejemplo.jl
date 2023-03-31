6eb8ef62bee1fe65621474805d8e50ae8b635802
AlaN <digitalforce.ac@gmail.com>
2023-03-30 20:47:05 -0600

    added 3rd grade estimation

--- Interpolación polinomial/Diferencias Newton/Julia/dp_ejemplo.jl
+++ Interpolación polinomial/Diferencias Newton/Julia/dp_ejemplo.jl
@@ -3,18 +3,22 @@ include("diferencias_prog.jl")
 function main()
 
     p_0 = Punto(50, 24.94)
-    p_1 = Punto(60, 20.11)
+    p_1 = Punto(60, 30.11)
     p_2 = Punto(70, 36.05)
-    p_3 = Punto(80, 23.84)
+    p_3 = Punto(80, 42.84)
     p_4 = Punto(90, 50.57)
     p_5 = Punto(100, 59.30)
 
     puntos = [p_0, p_1, p_2, p_3, p_4, p_5]
     est = 57.0
 
-    r = diferencias_newton(puntos, est, 2)
+    P_2 = diferencias_newton_prog(puntos, est, 2)
     println()
-    println(r)
+    println(P_2)
+    
+    P_3 = diferencias_newton_prog(puntos, est, 3)
+    println()
+    println(P_3)
 end
 
 main()
