99f43d4fab951bab74798065cf1b710ede2d2df8
ALaN <digitalforce.ac@gmail.com>
2023-04-02 13:43:45 -0600

    refactored lecturaPuntos; added clear for linux

--- Programas/Programa_2/C/main.c
+++ Programas/Programa_2/C/main.c
@@ -4,13 +4,17 @@
 #include "matriz.h"
 #include "diferencias_divididas.h"
 
-void lecturaPuntos(Punto *puntos, int *n_puntos)
+
+Punto * lecturaPuntos(int *n_puntos)
 {
+    Punto *puntos;
     int i = 0;
 
     printf("Introduce el numero de puntos> ");
     fflush(stdin);
     scanf("%d", n_puntos);
+
+    puntos = calloc(*n_puntos, sizeof(Punto));
 
     printf("Introduce los valores de los puntos\n");
 
@@ -22,23 +26,33 @@ void lecturaPuntos(Punto *puntos, int *n_puntos)
         printf("y_%d> ", i+1);
         fflush(stdin);
         scanf("%LF", &puntos[i].y);
-    }    
+    }
+
+    return puntos;
 }
 
 int main(void)
 {
+
     int i = 0;
     int n_puntos = 0;
     Punto *puntos;
 
-    lecturaPuntos(puntos, &n_puntos);
+    puntos = lecturaPuntos(&n_puntos);
 
-    system("cls");
+    #ifdef __linux__
+        system("clear");
+    #else
+        system("cls");
+    #endif
 
     for(i=0; i<n_puntos; i++)
     {
         printf("punto[%d] x = %LF y = %LF\n", i, puntos[i].x, puntos[i].y);
     }
 
+
+
+    free(puntos);
     return 0;
 }
