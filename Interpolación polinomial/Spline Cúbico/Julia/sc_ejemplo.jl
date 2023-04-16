9e763007ab784b2c856bf2b3cf8fa0ef1217c960
ALaN <digitalforce.ac@gmail.com>
2023-04-15 23:02:33 -0600

    holy shit, it works! ;-;

--- Interpolación polinomial/Spline Cúbico/Julia/sc_ejemplo.jl
+++ Interpolación polinomial/Spline Cúbico/Julia/sc_ejemplo.jl
@@ -1,37 +1,30 @@
+using Plots
+
 include("Spline_c.jl")
 
-function fcnFromString(s)
-    f = eval(Meta.parse("x -> " * s))
-    return x -> Base.invokelatest(f, x)
+p_0 = Punto(0.95, -1.1)
+p_1 = Punto(1.73, 0.27)
+p_2 = Punto(2.23, -0.29)
+p_3 = Punto(2.77, 0.56)
+p_4 = Punto(2.99, 1.0)
+#p_5 = Punto(3.33, 0.7)
+
+puntos = [p_0, p_1, p_2, p_3, p_4]
+
+g = Array{Function}(undef, length(puntos)-1)
+labels = Array{String}(undef, length(puntos)-1)
+
+g = Spline_C(puntos, true)
+
+for i ∈ axes(labels, 1)
+    labels[i] = @sprintf("g_%d(x)", i-1)
 end
 
-function main()
-    p_0 = Punto(0.95, -1.1)
-    p_1 = Punto(1.73, 0.27)
-    p_2 = Punto(2.23, -0.29)
-    p_3 = Punto(2.77, 0.56)
-    p_4 = Punto(2.99, 1.0)
-    #p_5 = Punto(3.33, 0.7)
+plot(xlabel="x", ylabel="y", title="polynomials", label=labels)
 
-    puntos = [p_0, p_1, p_2, p_3, p_4#=, p_5=#]
-
-    fn = Array{String}(undef, length(puntos)-1)
-    g = Array{Function}(undef, length(puntos)-1)
-
-    fn = Spline_C(puntos)
-
-    println()
-
-    for i ∈ axes(fn, 1)
-        println("Polinomio[", i, "] = ", fn[i])
-        g[i] = fcnFromString(fn[i])
-    end
-
-    display(g)
-
-    println(g[1](0.95))
-
-
+for i ∈ axes(g, 1)[begin:end]
+    x_a = range(puntos[i].x, puntos[i+1].x, length=100)
+    plot!(x_a, g[i], label=labels[i])
 end
 
-main()
+savefig("polynomials.png")
