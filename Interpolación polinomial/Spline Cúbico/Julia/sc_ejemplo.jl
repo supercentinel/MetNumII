bce5168f481e8757417e4d010d1f626b2c59dab8
ALaN <digitalforce.ac@gmail.com>
2023-04-15 19:52:23 -0600

    toying with eval

--- Interpolación polinomial/Spline Cúbico/Julia/sc_ejemplo.jl
+++ Interpolación polinomial/Spline Cúbico/Julia/sc_ejemplo.jl
@@ -1,7 +1,11 @@
 include("Spline_c.jl")
 
-function main()
+function fcnFromString(s)
+    f = eval(Meta.parse("x -> " * s))
+    return x -> Base.invokelatest(f, x)
+end
 
+function main()
     p_0 = Punto(0.95, -1.1)
     p_1 = Punto(1.73, 0.27)
     p_2 = Punto(2.23, -0.29)
@@ -11,7 +15,22 @@ function main()
 
     puntos = [p_0, p_1, p_2, p_3, p_4#=, p_5=#]
 
-    Spline_C(puntos)
+    fn = Array{String}(undef, length(puntos)-1)
+    g = Array{Function}(undef, length(puntos)-1)
+
+    fn = Spline_C(puntos)
+
+    println()
+
+    for i ∈ axes(fn, 1)
+        println("Polinomio[", i, "] = ", fn[i])
+        g[i] = fcnFromString(fn[i])
+    end
+
+    display(g)
+
+    println(g[1](0.95))
+
 
 end
 
