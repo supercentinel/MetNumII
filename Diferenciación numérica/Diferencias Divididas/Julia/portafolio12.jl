b869b9b38d0dbbd76beeccc6331deb76421175a0
ALaN <digitalforce.ac@gmail.com>
2023-05-07 17:48:21 -0600

    completed

--- Diferenciación numérica/Diferencias Divididas/Julia/portafolio12.jl
+++ Diferenciación numérica/Diferencias Divididas/Julia/portafolio12.jl
@@ -1,4 +1,4 @@
-include("../../Interpolación polinomial/Diferencias Divididas/Julia/diferencias_divididas.jl")
+include("ddiff_div.jl")
 
 function f(x::Float64)::Float64
     return (2 * x^2 - 1) * ((cos(4 * x))/(x))
@@ -13,4 +13,20 @@ puntos[4] = Punto(3.0, f(3.0))
 puntos[5] = Punto(3.4, f(3.4))
 puntos[6] = Punto(4.6, f(4.6))
 
+estimaciones = []
+
+x = 1.0
+
+for i ∈ 2:5
+    push!(estimaciones, diff_divididas(puntos, i, x, true))
+end
+
+errs = Array{Float64}(undef, length(estimaciones)-1)
+
+for i ∈ axes(errs, 1)
+    errs[i] = estimaciones[i+1] - estimaciones[i]
+end
+
 display(puntos)
+display(estimaciones)
+display(errs)
