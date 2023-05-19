184a52a2f34fdbf437e6c3c1e39d1540ba80c437
ALaN <digitalforce.ac@gmail.com>
2023-05-19 08:54:50 -0600

    added

---
+++ Integración Numérica/Julia/trapecio.jl
@@ -0,0 +1,20 @@
+struct Punto
+    x::Float64
+    y::Float64
+end
+
+function trapecio(puntos::Array{Punto}, outprint::Bool)
+    ∑f_i::Float64 = 0.0
+    h::Float64 = puntos[2].x - puntos[1].x
+
+    for i ∈ axes(puntos, 1)[begin+1:end-1]
+        println("f(x_i) = ", puntos[i].y)
+        ∑f_i += puntos[i].y
+    end
+
+    if outprint == true
+        println(h/2, " * ", puntos[1].y, " + ", puntos[length(puntos)].y, " + ", ∑f_i)
+    end
+
+    return (h/2) * (puntos[1].y + puntos[length(puntos)].y + (2 * ∑f_i))
+end
