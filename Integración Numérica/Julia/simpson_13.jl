0e8d21ea2fad51a21580323245a7ca9af1d62fe5
ALaN <digitalforce.ac@gmail.com>
2023-05-19 12:26:29 -0600

    completed

--- Integración Numérica/Julia/simpson_13.jl
+++ Integración Numérica/Julia/simpson_13.jl
@@ -3,34 +3,41 @@ struct Punto
     y::Float64
 end
 
-function simpson_13(puntos::Array{Punto}, a::Float64, b::Float64)::Float64
+function simpson_13(puntos::Array{Punto}, outprint::Bool)::Float64
     ∑s::Float64 = 0.0
-    i::Int64 = 1
-    n = length(puntos)
+    n = length(puntos)-1
     h = puntos[2].x - puntos[1].x
+    i::Int64 = 1
 
-    while i ≠ n-2
-        ∑s += ((4 * puntos[i].y) + (2 * puntos[i+1].y))
+    if outprint == true
+        s = string("f_0 + ")
+        ss = string(puntos[1].y, " + ")
+    end
+
+    while true
+
+        if outprint == true
+            s = string(s, "4f_", i, " + 2f_", i+1, " + ")
+            ss = string(ss, "4 * ", puntos[i+1].y, " + 2 * ", puntos[i+2].y, " + ")
+        end
+
+        ∑s += (4 * puntos[i+1].y) + (2 * puntos[i+2].y)
+
+        if i == n-3
+            break
+        end
+
         i += 2
     end
 
-    return  h/3 * puntos[1].y + puntos[length(puntos)].y + ∑s
+    if outprint == true
+        s = string(s, "4f_", i+2, " + f_", n)
+        ss = string(ss, "4 * ", puntos[n].y, " + ", puntos[n+1].y)
+        println(s)
+        println(ss)
+
+        println(h/3, " * [", puntos[1].y, " + ", puntos[n+1].y, " + ", ∑s, " + 4", puntos[i+2].y, "]")
+    end
+
+    return (h/3) * (puntos[1].y + puntos[n+1].y + ∑s + (4 * puntos[n].y))
 end
-
-function f(x::Float64)
-    return (5 * cos(x^3/2) + x^3 + log(x^2+1))
-end
-
-puntos = Array{Punto}(undef, 25)
-
-puntos[1] = Punto(-1.0, f(-1.0))
-
-for i ∈ 2:length(puntos)
-    x = puntos[i-1].x + 0.125
-    puntos[i]= Punto(x, f(x))
-end
-
-display(puntos)
-
-result = simpson_13(puntos, 0.0, 0.0)
-println("resultado = ", result)
