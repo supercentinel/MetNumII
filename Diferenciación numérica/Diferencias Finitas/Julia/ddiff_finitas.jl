73b943ec545eccb2b99a3b5cbf06faa68df75c9b
ALaN <digitalforce.ac@gmail.com>
2023-05-27 13:35:23 -0600

    refactored again for congruence of results

--- Diferenciación numérica/Diferencias Finitas/Julia/ddiff_finitas.jl
+++ Diferenciación numérica/Diferencias Finitas/Julia/ddiff_finitas.jl
@@ -5,11 +5,12 @@ end
 
 function ddiff_finitas(puntos::Array{Punto})
     h::Float64 = puntos[2].x - puntos[1].x
-    dfx = Array{Float64}(undef, length(puntos)-2)
+    dpuntos = Array{Punto}(undef, length(puntos)-2)
 
-    for i ∈ axes(dfx, 1)
-        dfx[i] = (1/(2 * h)) * (puntos[i+2].y - puntos[i].y)
+    for i ∈ axes(dpuntos, 1)
+        dfx = (1/(2 * h)) * (puntos[i+2].y - puntos[i].y)
+        dpuntos[i] = Punto(puntos[i+1].x, dfx)
     end
 
-    return dfx
+    return dpuntos
 end
